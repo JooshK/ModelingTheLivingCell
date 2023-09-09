8642830cc826b4a7bb0bc6de273f95f398f24117
JooshK <jokhorsandi@gmail.com>
2023-09-09 15:29:43 -0400

    PS2/2

---
+++ scripts/PS2/residue_sampling.py
@@ -0,0 +1,111 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+# dictionary of integers to amino acid one-letter codes
+amino_acid_keys = {
+    1: "A",
+    2: "R",
+    3: "N",
+    4: "D",
+    5: "C",
+    6: "Q",
+    7: "E",
+    8: "G",
+    9: "H",
+    10: "I",
+    11: "L",
+    12: "K",
+    13: "M",
+    14: "F",
+    15: "P",
+    16: "S",
+    17: "T",
+    18: "W",
+    19: "Y",
+    20: "V"
+}
+
+# percent composition in the Swiss prot KB database
+amino_acid_scale_values = {
+    "A":  8.25,
+    "R":  5.53,
+    "N":  4.06,
+    "D":  5.45,
+    "C":  1.37,
+    "Q":  3.93,
+    "E":  6.75,
+    "G":  7.07,
+    "H":  2.27,
+    "I":  5.96,
+    "L":  9.66,
+    "K":  5.84,
+    "M":  2.42,
+    "F":  3.86,
+    "P":  4.70,
+    "S":  6.56,
+    "T":  5.34,
+    "W":  1.08,
+    "Y":  2.92,
+    "V":  6.87
+}
+
+
+def uniformSampling(length):
+    samples = []
+    sequence = []
+
+    for i in range(length):
+        samples.append(np.random.randint(1, 21))  # uniform sampling of one of the 20 amino acids
+
+    for sample in samples:
+        sequence.append(amino_acid_keys[sample])  # convert to one-letter codes
+
+    return sequence
+
+
+def swissProtKBSampling(length):
+    amino_acids = list(amino_acid_scale_values.keys())
+    probs = list(amino_acid_scale_values.values())
+
+    return random.choices(amino_acids, probs, k=length)
+
+
+if __name__ == "__main__":
+    uniform_sequence = ''.join(uniformSampling(300))  # generate the sequence of 300 uniform amino acids
+    print(uniform_sequence)
+
+    # calculate the frequencies of each amino acid in the above sequence
+    freqs = {}
+    for amino_acid in amino_acid_keys.values():
+        freqs[amino_acid] = uniform_sequence.count(amino_acid)/300
+    print("Frequency of AA In Uniform Distribution")
+    print(freqs)
+
+    print("\n")
+
+    # sampling from the uniprot frequency data
+    actualSequence = ''.join(swissProtKBSampling(300))
+    print(actualSequence)
+
+    freqs = {}
+    for amino_acid in amino_acid_scale_values.keys():
+        freqs[amino_acid] = actualSequence.count(amino_acid)/300
+    print("Frequency of AA Sampled from SwissProtKB Database")
+    print(freqs)
+
+    fig, ax = plt.subplots()
+    ax.bar(amino_acid_scale_values.keys(), [a/100 for a in amino_acid_scale_values.values()])
+    plt.title("Amino Acid Frequency in SwissProt Database")
+    plt.xlabel("Amino Acid")
+    plt.ylabel("Sequence Frequency (%)")
+    plt.savefig("../../data/swissAAFreq.png")
+
+    # Answer to part d No, I don't think this sequence is realistic. Proteins have micro environments of amino acids
+    # with properties directed toward a task, the presence of one AA modifies the probability that one will follow
+    # next in the sequence. For example: Cys forms disulfide bridges that covalently hold some proteins together.
+    # While the probability of one Cys is low it increases significantly with the presence of another Cys residue.
+
+
+
+
