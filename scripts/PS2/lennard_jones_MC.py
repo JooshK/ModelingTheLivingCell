4dcfc4a0546833d1498dbf66bf465e230cc0c15a
JooshK <jokhorsandi@gmail.com>
2023-09-16 17:02:33 -0400

    Monte carlo simulation finished

--- scripts/PS2/lennard_jones_MC.py
+++ scripts/PS2/lennard_jones_MC.py
@@ -1,40 +1,92 @@
 import numpy as np
-from scipy.spatial.distance import pdist
-
-
-def calculate_distance_matrix(file, length):
-    """
-    Given a file of particle coordinates calculates the distance matrix subject to periodic boundary conditions
-    :param length: Length of box for PBCs
-    :param file: File of particle coords
-    """
-    coordinates = np.loadtxt(file)
-    n = len(coordinates)
-    distances = np.empty((n, n))
-
-    for i, particle_1 in enumerate(coordinates):
-        for j, particle_2 in enumerate(coordinates):
-            dist = 0
-            for component in range(len(particle_2)):
-                dist = abs(particle_2[component] - particle_1[component])  # calculate the distance per component
-                dist = abs(dist - length * round(dist/length))  # enforce PBC
-            distances[i, j] = dist
-
-    return distances
+import random
 
 
 def lennard_jones_potential(dist, e, sigma):
+    """
+    Calculates the lennard jones potential for a given distance r_ij
+    :param dist: The distance
+    :param e: energy
+    :param sigma: 'particle size'
+    :return: Lennard Jones potential energy
+    """
     return 4 * e * ((sigma / dist) ** 12 - (sigma / dist) ** 6)
 
 
-def calculate_lj_potential(file, e, sigma, length):
-    distances = calculate_distance_matrix(file, length)
-    potentials = []
-    for particle in distances:
-        for dist in particle:
-            if dist != 0:
-                potentials.append(lennard_jones_potential(dist, e, sigma))
-    return potentials
+def calculate_lj_potential(coordinates, e, sigma, length):
+    """
+    Given a file of coordinates calculates the LJ potential for the collection of particles with periodic boundary conditions
+    :param coordinates: Matrix of particle coordinates
+    :param e: Energy
+    :param sigma: Particle Size
+    :param length: Length of box for PBC
+    :return: LJ potential for particle configurations
+    """
+
+    potential = 0
+    for i in range(len(coordinates)):
+        for j in range(i+1, len(coordinates)):
+            r = coordinates[i] - coordinates[j]  # calculate pairwise distances
+            r -= length * np.round(r/length)  # Periodic Boundary
+            r = np.linalg.norm(r)
+
+            potential += lennard_jones_potential(r, e, sigma)
+    return potential
 
 
-print(calculate_distance_matrix("../../data/init_crds_boxl_3.5-2.dat", 3.5))
+def acceptance_probability(old_energy, new_energy, kbt):
+    """
+    The probability of accepting a transition to a new energy according to the
+    Boltzmann distribution
+    :param old_energy: Energy of x
+    :param new_energy: New state energy
+    :param kbt: temperature scale (defaults to 1)
+    :return: Acceptance probability
+    """
+    return min(1, np.exp((new_energy-old_energy)*-1/kbt))
+
+
+def monte_carlo(file, iterations, e, sigma, length, delta, kbt=1):
+    """
+    Runs the Markov Chain Monte Carlo simulation with the given parameters with Periodic Boundary Conditions
+    :param file: File containing initial coordinate data
+    :param iterations: Number of iterations to run the simulation
+    :param e: Energy (LJ Potential parameter)
+    :param sigma: Particle size (LJ Potential parameter)
+    :param length: Length of box
+    :param delta: Step size to make
+    :param kbt: The units of energy size, default to 1
+    :return: The average potential energy of the system
+    """
+    v_lj = np.zeros(iterations)  # pre-allocate the array for optimization
+    coordinates = np.loadtxt(file)
+    p_acc_total = np.zeros(iterations)
+
+    for i in range(iterations):
+        # randomly select a particle and get its state
+        selection = random.randint(0, len(coordinates)-1)
+        initial_state = coordinates[selection].copy()
+        if i > 0:  # only re calculate the energy after the first time (the LJ calculation is expensive)
+            initial_energy = v_lj[i - 1]
+        else:
+            initial_energy = calculate_lj_potential(coordinates, e, sigma, length)
+
+        # perform some potential move and find the new energy
+        for c in range(len(initial_state)):
+            coordinates[selection][c] += delta * (random.random() - 0.5)
+        new_state_energy = calculate_lj_potential(coordinates, e, sigma, length)
+
+        # get the acceptance probability
+        p_acc = acceptance_probability(initial_energy, new_state_energy, kbt)
+        p_acc_total[i] = p_acc
+
+        if random.random() < p_acc:  # if we accept the state change then keep the changed coordinates
+            v_lj[i] = new_state_energy
+        else:
+            coordinates[selection] = initial_state  # rejected state change, revert to old coordinates
+            v_lj[i] = initial_energy
+
+    print("Average acceptance probability:", p_acc_total.mean())
+    return sum(v_lj)/len(v_lj)
+
+
--- scripts/PS2/main.py
+++ scripts/PS2/main.py
@@ -1,13 +1,16 @@
+import numpy as np
+
 from gaussian_sampling import plot_sample_hist
 from residue_sampling import *
+from lennard_jones_MC import monte_carlo, calculate_lj_potential
 import matplotlib.pyplot as plt
 
 
-def response_1():
+def response1():
     plot_sample_hist(2000, 2, 3, 10, save=True, out_file="../../data/normalHist.png")
 
 
-def response_2():
+def response2():
     uniform_sequence = ''.join(uniform_sampling(300))  # generate the sequence of 300 uniform amino acids
     print(uniform_sequence)
 
@@ -38,6 +41,32 @@ def response_2():
     plt.savefig("../../data/swissAAFreq.png")
 
 
+def response3():
+    coordinate_data = r'../../data/init_crds_boxl_3.5-2.dat'
+    coords = np.loadtxt(coordinate_data)
+    e = 0.25
+    sigma = 1
+    l = 3.5
+    delta = 0.2
+    iterations = 100000
+
+    print("Initial Lennard Jones potential energy")
+    print(calculate_lj_potential(coords, e, sigma, l))
+
+    print("MCMC Simulation with kt = 1: ")
+    print("Average potential energy: ",monte_carlo(coordinate_data, iterations, e, sigma, l, delta), "Joules")
+
+    print("kt = 0.2")
+    print("Average potential energy: ", monte_carlo(coordinate_data, iterations, e, sigma, l, delta, 0.2), "Joules")
+
+    print("kt = 2")
+    print("Average potential energy: ", monte_carlo(coordinate_data, iterations, e, sigma, l, delta, 2), "Joules")
+
+    print("Box length l = 3.5, kt = 1: ")
+    print("Average potential energy: ", monte_carlo(coordinate_data, iterations, e, sigma, l, delta), "Joules")
+
+
 if __name__ == "__main__":
-    response_1()
-    response_2()
+    # response1()
+    # response2()
+    response3()
