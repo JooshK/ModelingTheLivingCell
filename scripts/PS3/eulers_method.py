85a00f83b56512ea71be7680b765a7546c961f8a
Joshua <jkhorsa1@jh.edu>
2023-09-26 14:47:27 -0400

    Finished problem 2

--- scripts/PS3/eulers_method.py
+++ scripts/PS3/eulers_method.py
@@ -1,12 +1,10 @@
 from typing import Callable
-import numpy as np
 
 
-def euler_method(func: Callable, step_size, t_0, t_f, y_0, n):
+def euler_method(func: Callable, step_size, t_0, y_0, n):
     """
     Runs euler method to approximate the solution for a differential equation
-    :param t_f: Final t value
-    :param func: the function dy/dt
+    :param func: the function dy/dt (depends only on y)
     :param step_size: Step size to take
     :param t_0: Initial value of t
     :param y_0: Initial value of y
@@ -14,3 +12,13 @@ def euler_method(func: Callable, step_size, t_0, t_f, y_0, n):
     :return: List of time steps and corresponding y values
     """
 
+    y = [y_0]
+    t = [t_0]
+    for i in range(n):
+        dy_dt = func(y[i])
+        y_next = y[i] + (step_size * dy_dt)
+        t_next = t[i] + step_size
+        print('%.4f\t%.4f\t%0.4f\t%.4f' % (t[i], y[i], dy_dt, y_next))
+        y.append(y_next)
+        t.append(t_next)
+    return t, y
