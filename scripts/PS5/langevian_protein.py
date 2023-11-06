c213ad9711f9ef30454494720d258ae1ee1e05d6
Joshua <jkhorsa1@jh.edu>
2023-11-06 17:05:18 -0500

    Configuration force for Langevian working correctly

--- scripts/PS5/langevian_protein.py
+++ scripts/PS5/langevian_protein.py
@@ -6,6 +6,10 @@ def harmonic_bond_potential(r, k, l):
     return 0.5*k*(abs(r) - l)**2
 
 
+def harmonic_bond_force(r, k, l):
+    return (-k * r * (abs(r) - l))/abs(r)
+
+
 def langevian_force(friction, m, kT, v, dt):
     stochastic_force = rng.normal(0, np.sqrt((2*kT*m*friction)/dt))
     return stochastic_force - friction*m*v
@@ -19,9 +23,9 @@ def verlet_propagation_velocity(friction, velocities, force, force_c, R, m, dt):
     velocities[:] = verlet_velocity_update(friction, velocities, m, force, force_c, R, dt)
 
 
-def calculate_configuration_force(coordinates, residue, epsilon, sigma, friction, m, k, l, kT, v, dt):
+def calculate_configuration_force(coordinates, residue, k, l):
     potential = 0
-    forces = np.zeros(len(coordinates))
+    forces = np.zeros((len(coordinates), 3))
 
     for i, particle_1 in enumerate(coordinates):  # loop over all the particles
         for j in range(i + 1, len(coordinates)):
@@ -35,12 +39,16 @@ def calculate_configuration_force(coordinates, residue, epsilon, sigma, friction
             # calculates the Lennard Jones potential contribution for nb residue
             if j != i + 1:
                 if residue_1 == 'H' and residue_2 == 'H':
-                    potential += lennard_jones_potential(r, epsilon, sigma)
+                    potential += lennard_jones_potential(r, epsilon=1, sigma=1)
+                    scalar_force = calculate_scalar_force(r, epsilon=1, sigma=1)
+                else:
+                    potential += lennard_jones_potential(r, epsilon=2/3, sigma=1)
+                    scalar_force = calculate_scalar_force(r, epsilon=2/3, sigma=1)
             else:  # bonded potential
                 potential += harmonic_bond_potential(r, k, l)
+                scalar_force = harmonic_bond_force(r, k, l)
 
             # calculates the vector force
-            scalar_force = calculate_scalar_force(r, epsilon, sigma) + langevian_force(friction, m, kT, v, dt)
             force = scalar_force * dist / r  # force in the radial direction, see
             # https://math.stackexchange.com/questions/1742524/numerical-force-due-to-lennard-jones-potential
 
